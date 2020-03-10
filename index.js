2f32f0a1da3b5fb247edb8e4d0e25ec4a7a8ee17
mdiaz26 <mike.diaz006@gmail.com>
2020-03-10 11:29:39 -0400

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Insert code here!!
+function myEach(arr, callback){
+    for(let i = 0; i < arr.length; i++){
+    const element = arr[i]
+    callback(element)
+    }
+    return arr
+}
+
+let callback = function(element){
+    return element ** 2
+}
+
+array = [2,4,6]
+
+function myMap(arr, callback){
+    let newArray= []
+    for(let i = 0; i < arr.length; i++){
+    const element = arr[i]
+    newArray.push(callback(element))
+    }
+    return newArray
+}
+
+function myFilter(arr, condition){
+    let newArray= []
+    for(let i = 0; i < arr.length; i++){
+    const element = arr[i]
+        if (condition(element)){
+            newArray.push(element)
+        }
+    }
+    return newArray
+}
+
+function myFind(arr, condition){
+    for(let i = 0; i < arr.length; i++){
+    const element = arr[i]
+        if (condition(element)){
+            return element
+        }
+    }
+}
+
